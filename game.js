479fd52a9a985b5fdc700cca6b1a4994c89a5adc
Harrison Ballard <harrisonwballard@gmail.com>
2020-08-04 19:01:18 -0400

    some minor changes

--- game.js
+++ game.js
@@ -2,9 +2,11 @@ const canvas = document.querySelector("#canvas");
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
 const WHITE = "rgb(255, 255, 255)";
+/*
 const RED = "rgb(255, 0, 0)";
-const MINXPOSIH = 0;
-const MINYPOSIH = 0;
+const MINXPOSITION = 0;
+const MINYPOSITION = 0;
+*/
 
 function makeRectangle (x,y,height,width,color, isPlayer) {
     return {
@@ -34,3 +36,37 @@ function drawRect(x,y,width,height,color) {
     ctx.fillStyle = color;
     ctx.fillRect(x,y,width,height);
 }
+/*
+function keyPressListener (event) {
+    if (event.key === "ArrowUp") {
+        player.y -= 10
+    } else if (event.key === "ArrowDown") {
+        player.y += 10
+    } else if (event.key === "ArrowRight") {
+        player.x += 10
+    } else if (event.key === "ArrowLeft") {
+        player.x -= 10
+    }
+    if (player.x > player.xUpper) {
+        player.x = player.xLower
+    } 
+    if (player.x < player.xLower) {
+        player.x = player.xUpper
+    }
+    if (player.y > player.yUpper) {
+        player.y = player.yLower
+    }
+    if (player.y < player.yLower) {
+        player.y = player.yUpper
+    }
+}
+
+
+function drawFrame () {
+    drawRect(player.x, player.y, player.width, player.height, player.color);
+}
+
+
+window.addEventListener("keydown", keyPressListener);
+
+*/
