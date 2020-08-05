c479c91371046ab3857311dcae16794fa3ddb20e
Harrison Ballard <harrisonwballard@gmail.com>
2020-08-04 20:38:23 -0400

    added the enemy immobile

--- game.js
+++ game.js
@@ -3,12 +3,9 @@ const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
 const WHITE = "rgb(255, 255, 255)";
 let animationID; 
-/*
 const RED = "rgb(255, 0, 0)";
-const MINXPOSITION = 0;
-const MINYPOSITION = 0;
-*/
 
+//this makes any rectangle, whether player or bad guy (may also include bullets when we get there)
 function makeRectangle (x,y,height,width,color, isPlayer) {
     return {
         x:x,
@@ -24,13 +21,25 @@ function makeRectangle (x,y,height,width,color, isPlayer) {
     }
 }
 
-let player = makeRectangle(200,200,30,30,WHITE,true)
+//initializes the player
+let player = makeRectangle(250,460,30,30,WHITE,true)
+//will change later to make randomized along y =5 and x = random between 0 and 500
+let badGuy = makeRectangle(randomInteger(30, 470), 40, 30, 30,RED,false)
 
+//helper function to randomize for badGuy spawns
+function randomInteger(min, max) {
+    let range = max - min + 1
+    let randomized = Math.floor(Math.random() * range)
+    return min + randomized
+}
+
+//makes it so you can have player and other stuff on canvas and interact with them
 function getContext() {
     return canvas.getContext("2d");
     
 }
 
+//creates the rectangle within the context
 function drawRect(x,y,width,height,color) {
     let ctx = getContext();
 
@@ -38,45 +47,34 @@ function drawRect(x,y,width,height,color) {
     ctx.fillRect(x,y,width,height);
 }
 
+//implements draw of the rectangle, not just the function
 drawRect(player.x, player.y, player.width, player.height, player.color);
+drawRect(badGuy.x, badGuy.y, badGuy.width, badGuy.height, badGuy.color);
 
-
+//makes it so there isn't a trailing history of movement for avatar
 function clearScreen() {
     let context = getContext();
     context.clearRect(0,0,WIDTH,HEIGHT);
 }
 
+//lets you move left and right
 function keyPressListener (event) {
-    if (event.key === "ArrowUp") {
-        player.y -= 10
-    } else if (event.key === "ArrowDown") {
-        player.y += 10
-    } else if (event.key === "ArrowRight") {
-        player.x += 10
-    } else if (event.key === "ArrowLeft") {
-        player.x -= 10
-    }
-    if (player.x > player.xUpper) {
-        player.x = player.xLower
+    if (event.key === "ArrowRight" && player.x < 455) {
+        player.x += 13
     } 
-    if (player.x < player.xLower) {
-        player.x = player.xUpper
-    }
-    if (player.y > player.yUpper) {
-        player.y = player.yLower
-    }
-    if (player.y < player.yLower) {
-        player.y = player.yUpper
+    else if (event.key === "ArrowLeft" && player.x > 5) {
+        player.x -= 13
     }
 }
 
-
+//creates frame by frame 
 function drawFrame () {
     clearScreen();
     drawRect(player.x, player.y, player.width, player.height, player.color);
+    drawRect(badGuy.x, badGuy.y, badGuy.width, badGuy.height, badGuy.color);
 }
 
-
+//allows for gameplay to start and stop, and movement listener
 window.addEventListener("keydown", keyPressListener);
 document.querySelector("#start-button").addEventListener("click", () => animationID = window.setInterval(drawFrame, 20))
 document.querySelector("#stop-button").addEventListener("click", () => window.clearInterval(animationID))
